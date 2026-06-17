6b41d62607e2d8043e0e79140b7a791196c2b49f
Hafizh <akhmadhafiz2016@gmail.com>
2026-06-17 18:34:20 +0800

    Fix: Disable PDO emulation to correctly handle LIMIT parameter

--- config/database.php
+++ config/database.php
@@ -25,6 +25,7 @@ try {
     $conn = new PDO($dsn, $db_user, $db_pass, [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
     ]);
 } catch (PDOException $e) {
